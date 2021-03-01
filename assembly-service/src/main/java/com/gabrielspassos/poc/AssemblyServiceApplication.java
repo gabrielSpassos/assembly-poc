d0d23fa82decc1973caf57a8e136fcf30f9ebeac
gabriel passos <gabriel.passos@agibank.com.br>
2021-03-01 19:26:49 -0300

    feat: add basic structure

---
+++ assembly-service/src/main/java/com/gabrielspassos/poc/AssemblyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielspassos.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssemblyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssemblyServiceApplication.class, args);
+	}
+
+}
